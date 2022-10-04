6dbeedcae99a0253c18de9b5346b17c9c849f161
Fionn <fionn010@gmail.com>
2022-10-04 19:04:50 +0100

    feat: added blog folder

---
+++ pages/blog/[id].tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/Layout";
+import { useRouter } from "next/router";
+
+const Post = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return (
+    <Layout>
+      <p>Post: {id}</p>
+    </Layout>
+  );
+};
+
+export default Post;

251c602f4a2d3a2b5cef9526a552d55670c0df1e
Fionn <fionn010@gmail.com>
2022-12-01 18:00:00 +0000

    feat: added dynamic routes. Struggling to display the blog content using dangerouslySetInnerHTML

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -41,3 +41,49 @@ export const getSortedPostsData = () => {
     }
   });
 };
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  // Returns an array that looks like this:
+  // [
+  //   {
+  //     params: {
+  //       id: 'ssg-ssr'
+  //     }
+  //   },
+  //   {
+  //     params: {
+  //       id: 'pre-rendering'
+  //     }
+  //   }
+  // ]
+
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ""),
+      },
+    };
+  });
+}
+
+// NOTE TO FIONN:
+
+// Figure out how to define what tyoe ID is here
+export function getPostData(id: string) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  const data = matterResult.data;
+  // Combine the data with the id
+  return {
+    id,
+    data,
+  };
+}
+
+// export default getAllPostIds;
